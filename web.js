3ece469f145c3f6e9bf591db1bfd1d8afec3a49f
Emayan Sundararajan <emayan25@gmail.com>
2013-07-14 19:24:59 +0000

    Added changes to web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var indexContent = fs.readfileSync(./bitstarter/index.html,function(err,data){
+var indexContent = fs.readfileSync(./index.html,function(err,data){
                       if(err) throw err});
 var buf = new Buffer(indexContent);
 
