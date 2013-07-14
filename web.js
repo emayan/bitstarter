84653f387ff9b64eba0204c62fa732fead64329b
Emayan Sundararajan <emayan25@gmail.com>
2013-07-14 23:37:44 +0000

    Added changes to web.js

--- web.js
+++ web.js
@@ -2,12 +2,11 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var indexContent = fs.readfileSync(./index.html,function(err,data){
-                      if(err) throw err});
+var indexContent = fs.readFileSync("./index.html","utf8");
 var buf = new Buffer(indexContent);
 
 app.get('/', function(request, response) {
-    response.send(buf.toString);
+    response.send(buf.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
