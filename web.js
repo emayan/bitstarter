10eda0e51a7bc84db757ad0c8fbe3bec5bd23de3
Emayan Sundararajan <emayan25@gmail.com>
2013-07-14 19:18:57 +0000

    Added chanes to web.js

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var indexContent = fs.readfileSync(./bitstarter/index.html,function(err,data){
+                      if(err) throw err});
+var buf = new Buffer(indexContent);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+    response.send(buf.toString);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
